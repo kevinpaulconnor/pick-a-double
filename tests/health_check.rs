38392e7cbd653a42157a19f2addc6a34c5239a3b
Kevin Connor <kevinpaulconnor@gmail.com>
2024-03-05 21:46:55 -0600

    use random available port for server and test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,12 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("http://{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -16,7 +16,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = pick_a_double::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = pick_a_double::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("127.0.0.1:{}", port)
 }
