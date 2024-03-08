050a255fad07e0fa2f5757b0b295b57a7b861aeb
Kevin Connor <kevinpaulconnor@gmail.com>
2024-03-07 21:00:57 -0600

    begin add select_player_game endpoint and tests (in progress)

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,22 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    game_id: i32,
+    player_id: i32,
+}
+
+async fn select_player_game(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| {
+        App::new().route("/health_check", web::get().to(health_check))
+        .route("/select_player_game", web::post().to(select_player_game))
+        })
         .listen(listener)?
         .run();
     Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -23,3 +23,58 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn select_player_game_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "email=ursula_le_guin%40gmail.com&game_id=1&player_id=1";
+
+    // Act
+    let response = client
+        .post(&format!("http://{}/select_player_game", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn select_player_game_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("", "missing email and game_id and player_id"),
+        ("email=ursula_le_guin%40gmail.com", "missing game_id and player_id"),
+        ("game_id=1", "missing email and player_id"),
+        ("player_id=1", "missing email and game_id"),
+        ("email=ursula_le_guin%40gmail.com&game_id=1", "missing player_id"),
+        ("email=ursula_le_guin%40gmail.com&player_id=1", "missing game_id"),
+        ("game_id=1&player_id=1", "missing email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("http://{}/select_player_game", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
