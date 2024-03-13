043c7ad26c4826955337fec49bd945d5cbf94ea2
Kevin Connor <kevinpaulconnor@gmail.com>
2024-03-12 21:32:58 -0500

    initialize configuration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use pick_a_double::startup::run;
 use std::net::TcpListener;
+use pick_a_double::startup::run;
+use pick_a_double::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
+    // Panic if we can't read config
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
