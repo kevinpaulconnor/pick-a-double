39e1cd619ed656a945d3e55e73c344d115784a93
Kevin Connor <kevinpaulconnor@gmail.com>
2024-03-12 19:04:17 -0500

    some error handling; db creation; wait until ready logic

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,21 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo 'Error: sqlx-cli is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v psql)" ]; then
+  echo 'Error: psql is not installed.' >&2
+  exit 1
+fi
+
 # Check if a customer user has been set, otherwise default to 'postgres
 DB_USER="${POSTGRES_USER:=postgres}"
 # Check if a custom password has been set, otherwise default to 'password'
@@ -22,3 +37,15 @@ docker run \
   -d postgres \
     postgres -N 1000
     # ^ Increased maximum number of connections for testing purposes
+
+# Keep pinging Postgres until it's ready to accept commands
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -d "${DB_NAME}" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on ${DB_HOST}:${DB_PORT}"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
