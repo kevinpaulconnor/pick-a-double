fc18e12f48e458a9fe8fe8f4add349b055880f62
Kevin Connor <kevinpaulconnor@gmail.com>
2024-03-20 22:57:59 -0500

    linting

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
     let address = format!(
         "{}:{}",
