1d0b8115355ac0c6fa88d973382f9f7cd63441b8
Kevin Connor <kevinpaulconnor@gmail.com>
2024-03-31 21:57:52 -0500

    randomized valid email validation

--- src/domain/user_email.rs
+++ src/domain/user_email.rs
@@ -27,6 +27,21 @@ impl AsRef<str> for UserEmail {
 mod tests {
     use super::UserEmail;
     use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -41,5 +56,10 @@ mod tests {
     #[test]
     fn email_missing_subject_is_rejected() {
         assert_err!(UserEmail::parse("@example.com".to_string()));
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        UserEmail::parse(valid_email.0).is_ok()
     }
 }
